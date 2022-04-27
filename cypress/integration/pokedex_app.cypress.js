3e23ee3c301251a40042a4879089c089978da0c6
miakauppila <mia.kauppila@gmail.com>
2022-04-27 21:34:05 +0300

    Add new e2e test (Ex. 11.9)

--- cypress/integration/pokedex_app.cypress.js
+++ cypress/integration/pokedex_app.cypress.js
@@ -4,4 +4,12 @@ describe('Pokedex', function () {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('user can navigate to a particular Pokemon page', function () {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('ivysaur')
+    cy.contains('overgrow')
+    cy.contains('chlorophyll')
+  })
 })
