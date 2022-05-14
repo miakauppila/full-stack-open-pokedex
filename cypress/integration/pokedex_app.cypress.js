6ba6d1639a98f2bf1fca7014cec74ba69f9db802
miakauppila <mia.kauppila@gmail.com>
2022-05-14 12:55:29 +0300

    Testing failure notification (Ex. 11.18)

--- cypress/integration/pokedex_app.cypress.js
+++ cypress/integration/pokedex_app.cypress.js
@@ -2,7 +2,7 @@ describe('Pokedex', function () {
   it('front page can be opened', function () {
     cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
-    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintend.')
   })
 
   it('user can navigate to a particular Pokemon page', function () {
